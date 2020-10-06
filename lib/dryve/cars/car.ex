2e92feebc2289fd0fab6970082c4bd981182cdf3
Ryan Zidago <ryan.zidago@studitemps.de>
2020-10-06 16:40:29 +0200

    wip

--- lib/dryve/cars/car.ex
+++ lib/dryve/cars/car.ex
@@ -2,6 +2,8 @@ defmodule Dryve.Cars.Car do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Dryve.Repo
+
   @doc """
   A car has
     * engine_type
@@ -38,5 +40,23 @@ defmodule Dryve.Cars.Car do
     car
     |> cast(params, @allowed_fields)
     |> check_constraint(:engine_type, name: :mutually_exclusive)
+  end
+
+  def adjacent_prices do
+    query = """
+      WITH base AS (
+        SELECT lag(price, 2) OVER (ORDER BY price) lag,
+        lead(price, 2) OVER (ORDER BY price) lead,
+        c.*
+        FROM cars c
+      )
+      SELECT base.* FROM base
+      JOIN(
+        SELECT lag, lead
+        FROM base WHERE price = 5000
+      ) sub ON base.price BETWEEN sub.lag AND sub.lead
+    """
+
+    Repo.query!(query)
   end
 end
